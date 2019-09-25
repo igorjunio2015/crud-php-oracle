d97529871bf8882b2022982cdf26f657d25d2fdc
igorjunio2015 <igorjunio2015@gmail.com>
2019-09-25 15:10:12 -0300

    CRUD ORACLE

--- api.php
+++ api.php
@@ -6,6 +6,10 @@ $method = $_SERVER['REQUEST_METHOD'];
 header('Content-type: application/json');
 $body = file_get_contents('php://input');
 
+$id_empresa = "";
+$chapa = "";
+$lmover = "";
+
 if ($method === 'GET') {
     $path = explode('/', $_GET['chapa']);
     if ($path[0]) {
@@ -21,28 +25,34 @@ if ($method === 'GET') {
                 JSON_PRETTY_PRINT
             );
         }
-        oci_close($conn);
     } else {
         echo json_encode("Path is required 'CHAPA'", JSON_PRETTY_PRINT);
     }
 } else if ($method === 'POST') {
+    //method POST    
+    //https://api.com/users
+    /*  {
+            "ID_EMPRESA": "2",
+            "CHAPA": "17594",
+            "LMOVER": "igor.melo.teste"
+        }
+    */
     $jsonBody = json_decode($body, true);
 
     $message = [];
     if (!isset($jsonBody["ID_EMPRESA"])) {
-        $id_empresa = "";
         array_push($message, "IDEMPRESA is required in body.");
     } else {
         $id_empresa = $jsonBody["ID_EMPRESA"];
     }
+
     if (!isset($jsonBody["CHAPA"])) {
-        $chapa = "";
         array_push($message, "CHAPA is required in body.");
     } else {
         $chapa = $jsonBody["CHAPA"];
     }
+
     if (!isset($jsonBody["LMOVER"])) {
-        $lmover = "";
         array_push($message, "LMOVER is required in body.");
     } else {
         $lmover = $jsonBody["LMOVER"];
@@ -60,16 +70,16 @@ if ($method === 'GET') {
                              dual
                         where not exists";
         $query_select = "(
-                         select 
-                                 *
-                         from 
-                                 sys.login_sistema sls
-                         where
+                        select 
+                                *
+                        from 
+                                sys.login_sistema sls
+                        where
                                 (sls.id_empresa = '$id_empresa'
-                         and
-                                 sls.chapa = '$chapa'
-                         and
-                                 sls.lmover = '$lmover'))";
+                        and
+                                sls.chapa = '$chapa'
+                        and
+                                sls.lmover = '$lmover'))";
 
         $statemen = oci_parse($conn, $query_select);
         $result_sql = oci_execute($statemen);
@@ -88,7 +98,6 @@ if ($method === 'GET') {
             }
         }
     }
-    oci_close($conn);
 } else if ($method === 'PUT') {
     //method PUT    
     //https://api.com/users?id_empresa=X&chapa=XXXXX&lmover=XXXXX
@@ -98,39 +107,76 @@ if ($method === 'GET') {
             "LMOVER": "igor.melo.teste"
         }
     */
-
     $jsonBody = json_decode($body, true);
 
     $message = [];
+    // Param and Body = ID_EMPRESA
     if (!isset($jsonBody["ID_EMPRESA"]) || !isset($_GET["ID_EMPRESA"])) {
-        $id_empresa = "";
         array_push($message, "ID_EMPRESA is required in body.");
     } else {
         $param_id_empresa = $_GET['ID_EMPRESA'];
         $id_empresa = $jsonBody["ID_EMPRESA"];
     }
+    // Param and Body = CHAPA
     if (!isset($jsonBody["CHAPA"]) || !isset($_GET["CHAPA"])) {
-        $chapa = "";
         array_push($message, "CHAPA is required in body and param.");
     } else {
         $param_chapa = $_GET['CHAPA'];
         $chapa = $jsonBody["CHAPA"];
     }
+    // Param and Body = LMOVER
     if (!isset($jsonBody["LMOVER"]) || !isset($_GET["LMOVER"])) {
-        $lmover = "";
         array_push($message, "LMOVER is required in body and param.");
     } else {
         $param_lmover = $_GET['LMOVER'];
         $lmover = $jsonBody["LMOVER"];
     }
 
-
-
     if (count($message) != 0) {
         echo json_encode($message, JSON_PRETTY_PRINT);
     } else {
-        echo "else PUT";
+        $query_update = "update sys.login_sistema sls
+                        set
+                             sls.id_empresa ='$id_empresa'
+                            ,sls.chapa ='$chapa'
+                            ,sls.lmover ='$lmover'
+                        where
+                            (sls.id_empresa = '$param_id_empresa'
+                        and
+                             sls.chapa = '$param_chapa'
+                        and
+                             sls.lmover = '$param_lmover')";
+        $query_select = "(
+                        select 
+                                *
+                        from 
+                                sys.login_sistema sls
+                        where
+                                (sls.id_empresa = '$param_id_empresa'
+                        and
+                                sls.chapa = '$param_chapa'
+                        and
+                                sls.lmover = '$param_lmover'))";
+                                
+
+        $statemen = oci_parse($conn, $query_select);
+        $result_sql = oci_execute($statemen);
+
+        $row = oci_fetch_array($statemen);
+
+        if ($row[1] != $chapa) {
+            print json_encode(["Not Exists" => "Value is not exists in database, check."], JSON_PRETTY_PRINT);
+        } else {
+            $statemen = oci_parse($conn, ($query_update));
+            $result_sql = oci_execute($statemen);
+            if ($result_sql) {
+                print json_encode(["Success" => "Data successfully updated into database.."], JSON_PRETTY_PRINT);
+            } else {
+                print json_encode(["Error" => "Could not updated data into database, check."], JSON_PRETTY_PRINT);
+            }
+        }
     }
 } else {
     echo json_encode("Method not permited", JSON_PRETTY_PRINT);
 }
+oci_close($conn);
