47982c80cd34960bd6300a8e52b756799712bb8b
igorjunio2015 <igorjunio2015@gmail.com>
2019-09-27 16:37:41 -0300

    CRUD ORACLE MVC PHP DEFAULT

--- action/actions.php
+++ action/actions.php
@@ -28,7 +28,6 @@ class Listar
         return 'Listar';
     }
 }
-
 class Incluir
 {
     public function __construct()
@@ -44,30 +43,40 @@ class Incluir
         $jsonBody = json_decode($body, true);
 
         $message = [];
+        // set IDEMPRESA
         if (!isset($jsonBody["ID_EMPRESA"])) {
             array_push($message, "IDEMPRESA is required in body.");
         } else {
             $idEmpresa = $jsonBody["ID_EMPRESA"];
         }
-
+        // set CHAPA
         if (!isset($jsonBody["CHAPA"])) {
             array_push($message, "CHAPA is required in body.");
         } else {
             $chapa = $jsonBody["CHAPA"];
         }
-
+        // set LMOVER
         if (!isset($jsonBody["LMOVER"])) {
             array_push($message, "LMOVER is required in body.");
         } else {
             $lmover = $jsonBody["LMOVER"];
         }
 
-        if (count($message) != 0) {
-            echo json_encode($message, JSON_PRETTY_PRINT);
-        } else {
-            $dao->inserir($conexao, array(
+        if (count($message) === 0) {
+            // check values exists in database
+
+            $checarExiste = $dao->procurar($conexao, array(
                 'idEmpresa' => $idEmpresa, 'chapa' => $chapa, 'lmover' => $lmover
             ));
+            if (!empty($checarExiste)) {
+                echo json_encode(["Exists" => "The user already exists in the database."], JSON_PRETTY_PRINT);
+            } else {
+                $dao->inserir($conexao, array(
+                    'idEmpresa' => $idEmpresa, 'chapa' => $chapa, 'lmover' => $lmover
+                ));
+            }
+        } else {
+            echo json_encode($message, JSON_PRETTY_PRINT);
         }
         return 'Incluir';
     }
--- loginSoftExpertDAO.php
+++ loginSoftExpertDAO.php
@@ -2,9 +2,51 @@
 class LoginSoftExpertDAO
 {
     private $db;
+    
     public function __construct(Database $db)
     {
         $this->db = $db;
+    }
+
+    public function procurar($conexao, $parametros)
+    {
+        try {
+            if (!empty($parametros['chapa'])) {
+                if (!empty($parametros['chapa'])) {
+                    $sql = "
+                    select 
+                         sls.id_empresa
+                        ,sls.chapa
+                        ,sls.lmover
+                    from 
+                         SYS.login_sistema sls 
+                    where 
+                         sls.chapa like '%" . $parametros['chapa'] . "%'";
+                }
+                if (!empty($parametros['idEmpresa'])) {
+                    $sql .= " and sls.id_empresa = " . $parametros['idEmpresa'] . "";
+                }
+                if (!empty($parametros['lmover'])) {
+                    $sql .= " and sls.lmover = '" . $parametros['lmover'] . "'";
+                }
+                $query = oci_parse($conexao, $sql);
+                oci_execute($query);
+                while ($row = oci_fetch_array($query)) {
+                    $array = json_encode([
+                        "COD_EMPRESA" => $row[0],
+                        "CHAPA" => $row[1],
+                        "LOGIN_MOVER" => $row[2]
+                    ], JSON_PRETTY_PRINT);
+                }
+                if (isset($array)) {
+                    return $array;
+                }
+            } else {
+                print json_encode("Check params, 'ID_EMPRESA', 'CHAPA', 'LMOVER' is required.", JSON_PRETTY_PRINT);
+            }
+        } catch (Exception $e) {
+            return $e;
+        }
     }
 
     public function inserir($conexao, $parametros)
@@ -46,46 +88,39 @@ class LoginSoftExpertDAO
                 } else {
                     print json_encode(["Error" => "Could not insert data into database, check."], JSON_PRETTY_PRINT);
                 }
+            } else {
+                print json_encode("Check body scope, 'ID_EMPRESA', 'CHAPA' and 'LMOVER' is required.", JSON_PRETTY_PRINT);
             }
         } catch (Exception $e) {
             return $e;
         }
     }
 
-    public function procurar($conexao, $parametros)
+    public function modificar($conexao, $parametros)
     {
         try {
-            if (!empty($parametros['chapa'])) {
-                if (!empty($parametros['chapa'])) {
-                    $chapa = $parametros['chapa'];
-                    $sql = "select 
-                         sls.id_empresa
-                        ,sls.chapa
-                        ,sls.lmover
-                    from 
-                         SYS.login_sistema sls 
-                    where 
-                         sls.chapa like '%$chapa%'";
-                }
-                if (!empty($parametros['idEmpresa'])) {
-                    $idEmpresa = $parametros['idEmpresa'];
-                    $sql .= "and sls.id_empresa = $idEmpresa";
-                }
-                if (!empty($parametros['lmover'])) {
-                    $lmover = $parametros['lmover'];
-                    $sql .= "and sls.id_empresa = $lmover";
-                }
+            if (
+                !empty($parametros['idEmpresa'] &&
+                    !empty($parametros['chapa'] &&
+                        !empty($parametros['lmover'])))
+            ) {
+                $sql = "
+                    update
+                        sys.login_sistema sls
+                    set 
+                        sls.id_empresa = 3
+                        ,sls.chapa = '17594'
+                        ,sls.lmover = 'igor.teste_'
+                    where
+                            sls.id_empresa = " . $parametros['idEmpresa'] . "
+                    and
+                            sls.chapa = '" . $parametros['chapa'] . "'
+                    and
+                            sls.lmover = '" . $parametros['lmover'] . "'";
                 $query = oci_parse($conexao, $sql);
                 oci_execute($query);
-                while ($row = oci_fetch_array($query)) {
-                    print json_encode([
-                        "COD_EMPRESA" => $row[0],
-                        "CHAPA" => $row[1],
-                        "LOGIN_MOVER" => $row[2]
-                    ], JSON_PRETTY_PRINT);
-                }
             } else {
-                print json_encode("Path is required 'CHAPA'", JSON_PRETTY_PRINT);
+                print json_encode("Check body scope, 'ID_EMPRESA', 'CHAPA' and 'LMOVER' is required.", JSON_PRETTY_PRINT);
             }
         } catch (Exception $e) {
             return $e;
